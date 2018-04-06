5d972996a311fb9ba7fdb161fc6f41a9717d19eb
alexorear <alex.orear@gmail.com>
2018-04-05 22:02:17 -0500

    Set up controls to allow player to move left, right or jump.

--- js/main.js
+++ js/main.js
@@ -23,10 +23,31 @@ var mainState = {
 
 		// add physics engine to all game objects
 		game.world.enableBody = true;
+
+		// variable to store arrow key press
+		this.curser = game.input.keyboard.createCursorKeys();
+
+		// create play in middle of Game
+		this.player = game.add.sprite(70, 100, 'player')
+
+		// add gravity to effect player
+		this.player.body.gravity.y = 600;
 	},
 
 	update: function() {
+		// move the player when an arrow key is pressed
+		if (this.curser.left.isDown) {
+			this.player.body.velocity.x = -200;
+		} else if (this.curser.right.isDown) {
+			this.player.body.velocity.x = 200;
+		} else {
+			this.player.body.velocity.x = 0;
+		}
 
+		// let the player jump if they are touching the ground
+		if (this.curser.up.isDown && this.player.body.touching.down) {
+			this.player.body.velocity.y = -200;
+		}
 	}
 };
 
